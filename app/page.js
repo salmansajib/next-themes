0af0d5b51e239c60a82e098ddd5b8057b97b5dac
Salman-sajib <sfsajib@gmail.com>
2025-01-26 13:52:02 +0600

    animation duration

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import ThemeSwicher from "@/components/ThemeSwicher";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-mono)] bg-teal-50 dark:bg-zinc-950 text-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
       <header className="border-b border-gray-400/50 dark:border-gray-50/20 flex items-center justify-center w-full h-20">
-        <ThemeSwicher />
+        <ThemeSwitcher />
       </header>
 
       <main className="container mx-auto px-4 my-10">
--- components/ThemeSwitcher.js
+++ components/ThemeSwitcher.js
@@ -20,7 +20,7 @@ function ThemeSwicher() {
       animate={{ opacity: 1, scale: 1 }}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.1 }}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="flex items-center gap-3 border border-gray-400/50 dark:border-gray-50/20 px-5 py-2 rounded-md"
     >
